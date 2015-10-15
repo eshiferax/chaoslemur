e5f6e1c581dffb6b899ff778e625413c68cdc53b
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-10-15 02:45:24 -0400

    add script to prepare images for docker containers

---
+++ prepare_images.sh
@@ -0,0 +1,9 @@
+sudo docker pull gliderlabs/alpine
+sudo docker images gliderlabs/alpine
+
+git clone git@github.com:spacedog/docker-quagga
+cd docker-quagga
+cat Dockerfile
+
+sudo docker pull abaranov/quagga
+
