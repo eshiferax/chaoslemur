6bfec0a2dfb320e98b0aeb088545be0b9f196966
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-11-02 13:46:49 +0000

    setup process for ubu image works. next step = get container id programatically for pipework

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+# Automated setup of ubuntu docker environment for ChaosLemur
+# Emmanuel Shiferaw
+# Davis Gossage
+
+###
+# install pipework, lxterminal
+###
 sudo bash -c "curl https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework > /usr/local/bin/pipework"
+PATH=$PATH:/usr/local/bin/pipework
 sudo apt-get install lxterminal
 
+###
+# build Docker image from
+###
 sudo docker build -t quagga .
-
