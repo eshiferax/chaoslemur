eef5eae2ecf46e2ddd7abef3cafcd9e1f29116b5
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-11-09 02:58:55 +0000

    base imagegenerator

--- src/ConfigGen/ImageGenerator.py
+++ src/ConfigGen/ImageGenerator.py
@@ -6,6 +6,7 @@
 import subprocess
 import json
 from docker import Client
+import os
 
 ###
 cli = Client(base_url='unix://var/run/docker.sock')
@@ -20,9 +21,14 @@ cli = Client(base_url='unix://var/run/docker.sock')
 def generateImages(path):
 
     ## TODO: Get all directories at path
+    #lsProc = subprocess.Popen("ls", stdout=subprocess.PIPE)
+    #(lsResponse, err) = lsProc.communicate()
+    allDirs = os.listdir(path)
 
+    imageList = []
     ## TODO: Loop through directories, building image for each
-
+    for dir in allDirs:
+        currResponse = cli.build(path + dir)
 
 
 ###
