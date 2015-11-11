cf995982e5d8334d6b85db54998b873b9d67b80f
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-11-11 01:42:20 +0000

    bgpd.conf template for ConfigGenerator

--- src/ConfigGen/ConfigGenerator.py
+++ src/ConfigGen/ConfigGenerator.py
@@ -6,6 +6,11 @@
 import subprocess
 import json
 
+with open ("bgpd.conf.template", "r") as tfile:
+   bgpd_template = tfile.readlines()
+   router_portion = bgpd_template[13:19] # get portion of conf template listing router IP, neighbors
+
+
 ###
 # Given desired number of routers and topology, will generate
 # bgpd.conf quagga BGP configuration files and place them in specific location
@@ -20,11 +25,13 @@ def generateConfigs(num_routers, topology, path):
     ## Get docker network inspection info on default 'bridge' docker network
     inspectProc = subprocess.Popen(["sudo", "docker", "network", "inspect", "bridge"], stdout=subprocess.PIPE)
     (inspectResponse, err) = inspectProc.communicate()
-    
+    jsonResp = json.loads(inspectResponse)    
+
     ## TODO: Extract subnet/gateway from JSON response
 
     ## TODO: Build bgpd.conf string for each router, save
-
+   
+    print router_portion
     print inspectResponse
 
-generateConfigs(1,2)
+generateConfigs(1,2,".")
--- src/ConfigGen/ImageGenerator.py
+++ src/ConfigGen/ImageGenerator.py
@@ -36,4 +36,4 @@ def generateImages(path):
 ###
 def generateContainers(images):
 
-~                    
+                    
