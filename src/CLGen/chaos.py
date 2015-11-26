ef3f982f37054ce7dc1498619ee903f3a1320ac2
Emmanuel Shiferaw <eshiferax@gmail.com>
2015-11-26 18:12:56 +0000

    method to take down random node. method to recover all failures

--- src/CLGen/chaos.py
+++ src/CLGen/chaos.py
@@ -7,11 +7,13 @@
 # Contains classes used to destroy
 
 import datetime
+import random
 
 class ChaosLemur:
 
-    def __init__(self):
-        self.failure_event_list = [] # (event, time)
+    def __init__(self, num_routers):
+        self.event_list = [] # (event, time)
+        self.num_routers = num_routers
 
     ###
     # Take down Node
@@ -19,25 +21,36 @@ class ChaosLemur:
     def takeDownNode(self, num):
         tdCommand = "sudo docker stop quag%s" % (num)
         os.system(tdCommand)
-        self.failure_event_list.append((num, datetime.datetimenow()))
+       
+        failure_event = ChaosLemurEvent("Failure", datetime.datetime.now()) 
+        self.event_list.append(failure_event)
 
     ###
     # Reverse all failures
     ###
     def reverseFailures(self):
         
-        for event in self.failure_event_list:
+        for event in self.event_list:
             # TODO: Implement
-            print "Reversing failure: "
+            if event.event_type == "Failure":
+                recover_command = "sudo docker start quag%s" % (event.num)
+                os.system(recover_command)
+                print "Reversing failure for node %s" % (event.num)
 
-
-
+    ###
+    # Take down RANDOM node
+    ###
+    def takeDownRandomNode(self):
+        
+        node_to_fail = random.randint(0, self.num_routers)
+        self.takeDownNode(node_to_fail)
 
 class ChaosLemurEvent:
 
-    def __init__(self, typ, time):
+    def __init__(self, typ, time, number):
         self.event_type = typ
         self.time = time
+        self.num = number
 
 
 
